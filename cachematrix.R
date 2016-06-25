eba7971e0ed2a9d588af60eb3d337a94423c7c1a
collnell <coll.s.nell#gmail.com>
2016-06-24 17:28:51 -0700

    functions work, test, confused about what makes a matrix singer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## caching the inverse of a matrix
+## saves time! avoids repeating computations
 
-## Write a short comment describing this function
-
+## first, write a function creates a special "matrix" object that can cache its inverse
+##result should return a list of functions that: set matrix, get matrix, set inverse, get inverse
+#then put that into cacheSolve ?
+##so x needs to be matrix(c(values),nrow=row,ncol=col)
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverz <- NULL
+  set <-function(y=matrix()) {
+    x <<- y ##cache
+    inverz<<- NULL
+  }
+  get = function() x
+  setinverz = function(inverse) inverz <<- inverse
+  getinverz = function() inverz
+  list(set=set, get=get, setinverz=setinverz, getinverz=getinverz)
 }
 
 
-## Write a short comment describing this function
+## then a function that computes the inverse of the matrix from above
+##if the inverse has been calculated and the matrix is the same, pull inverse from cache
+##otherwise compute
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverz<- x$getinverz()
+  if(!is.null(inverz)){
+    message("getting cached data")
+    return(inverz)
+  }
+  data.matrix<-x$get()
+  inverz<-solve(data.matrix, ...)
+  x$setinverz(inverz)
+  return(inverz)
 }
+
+
+##test it out
+d=rnorm(60)
+test.matrix=matrix(c(1,0,0,1),nrow=2,ncol=2)
+test.matrix
+
+cache.matrix<-makeCacheMatrix(test.matrix)
+cacheSolve(cache.matrix)
+
+det(test.matrix)
+
